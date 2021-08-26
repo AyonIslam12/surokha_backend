0415520611dfbc4d74c1c96dcd0d09eb2ec1c443
Md.Mehedi Hasan <61658463+AyonIslam12@users.noreply.github.com>
2021-08-26 23:56:45 +0600

    create api controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -92,4 +92,5 @@ class CategoryController extends Controller
     {
         //
     }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController as ApiCategoryController;
 use App\Http\Controllers\Api\VerificationController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +21,7 @@ Route::get('/test', function(){
     return 'I am from backend ok';
 });
 Route::post('/verify',[VerificationController::class,'verify']);
+Route::get('/categories',[ApiCategoryController::class,'getCategories']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
